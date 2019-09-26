3d54b29446b742088d006f404aa34fbdbff99790
Mantas Andriukevicius <mantuxas97@gmail.com>
2019-09-26 07:56:37 +0300

    inicial commit

---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        ivaz-project
+      </h1>
+      <h2 class="subtitle">
+        ivaz project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <i class="material-icons white-text">error_outline</i>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+
+$font-stack: 'RobotoBold';
+$primary-color: rgb(224, 0, 0);
+
+body {
+  font: 100% $font-stack;
+  color: $primary-color !important;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  // font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  //   'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: $primary-color;
+  font-family: $font-stack;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/prisijungti/registracija/index.vue
@@ -0,0 +1,43 @@
+<template>
+<section>
+
+    <b-container class="bv-example-row bv-example-row-flex-cols">
+        <b-row align-v="start">
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+        </b-row>
+
+        <b-row align-v="center">
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+        </b-row>
+
+        <b-row align-v="end">
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+            <b-col>One of three columns</b-col>
+        </b-row>
+    </b-container>
+
+</section>
+</template>
+
+<style lang="scss">
+$primary-color: #2699FB;
+
+
+html, body{
+    overflow: hidden;
+}
+section{
+    height: 100vh;
+}
+.i-title-box{
+    background-color: $primary-color;
+    min-height: 22%;
+    // align-items: flex-end;
+}
+
+</style>
